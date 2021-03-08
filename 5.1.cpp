9f7e7583de0283ca8116291f65e7134dad32a90c
b6320502517 <sarannut.l@ku.th>
2021-03-08 18:58:09 +0700

    3

--- 5.1.cpp
+++ 5.1.cpp
@@ -1,26 +1,27 @@
 #include<stdio.h>
 int main(){
-	int n,m,i,j,l=0,k,h,w,i1,j1,l1=1;
+	int n,m,i,j,l=0,k,h,w,i1,j1,l1=0,w1;
 	scanf("%d",&n);
 	scanf("%d",&m);
-	int a[m][m]={},b[n][n]={},c[20]={};
+	int a[n][n]={}; int c[20]={};
 	for(i=0;i<n;i++)
 		for(j=0;j<n;j++)
 			scanf("%d",&a[i][j]);
-	w=n-(m-1);
+	w=m,w1=m;
 	i1=0,j1=0;
 	while(1){
-		for(i=i1;i<n-(m-1);i++){
+		for(i=i1;i<w1;i++){
 			for(j=j1;j<w;j++){
 				c[l]+=a[i][j];
-			printf("%d %d",a[i][j],c[l]);
+			printf("%d ",a[i][j]);
 			}
 			l1++;
 		}
 		w++,l++,j1++;
 		if(l1%m==0){
 			i1++;
-			w=n-(m-1);
+			w=m;
+			w1++;
 			j1=0;
 			
 		}
@@ -37,8 +38,8 @@ int main(){
 			k=c[i];
 			h=i;
 		}
-	}*/
-	//printf("%d",c[h]);
+	}
+	printf("%d",c[h]);*/
 
 	return 0;
 }
