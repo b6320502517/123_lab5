ea6ed3c9a449d6eb4602ccdbec672ba453900ea4
b6320502517 <sarannut.l@ku.th>
2021-03-08 17:32:49 +0700

    2

--- 2.cpp
+++ 2.cpp
@@ -1,8 +1,16 @@
 #include<stdio.h>
 int main(){
-	int i,j,k,n=1;
+	int i,j,k=0,l,n=1;
 	if(n<=1000&&n>=1)
 		scanf("%d",&n);
-  
+  if(n%2==0){
+  	n--;
+  	k++;
+  	l=((n+k)/2)-1;
+  }
+  else{
+  	l=((n+k)/2);
+  	
+  }
 
 }
