e02e3324c18ec111a0cdbec28399821f3c4e2208
b6320502517 <sarannut.l@ku.th>
2021-03-08 19:13:22 +0700

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,11 +3,14 @@ int main(){
 	int n,i,j,k,l=1;
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
-		if(l%2==0||l%3==0||l%5==0||l%7==0||l==1||l==2||l==5){
+		if((l%2==0||l%3==0||l%5==0||l%7==0||l==1||l==2||l==5)&&(l!=7)){
 			l++;
 		}
-		else
+		else{
 			i--;
+			l++;
+		}
+			
 	}
 	printf("%d",l);
 	return 0;
